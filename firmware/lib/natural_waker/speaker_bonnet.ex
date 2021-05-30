277b5458b5443cdbdc274887cf83afc2807b8640
Jason <jason@fullsteamlabs.com>
2021-05-29 20:51:48 -0400

    Adds support for audio repeat

--- firmware/lib/natural_waker/speaker_bonnet.ex
+++ firmware/lib/natural_waker/speaker_bonnet.ex
@@ -2,6 +2,10 @@ defmodule NaturalWaker.SpeakerBonnet do
   require Logger
   use GenServer
 
+  defmodule State do
+    defstruct [:file, :repeat_file]
+  end
+
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: SpeakerBonnet)
   end
@@ -10,25 +14,31 @@ defmodule NaturalWaker.SpeakerBonnet do
   def init(_opts) do
     System.cmd("aplay", ["/etc/silence.wav"])
     Process.send({VolumeManager, Node.self()}, {:set_volume, 0}, [])
-    {:ok, %{}}
-  end
 
-  @impl GenServer
-  def handle_info(:start_audio, state) do
-    Task.start(fn -> System.cmd("aplay", ["/root/audio/birds.wav"]) end)
-    {:noreply, state}
+    state = %State{
+      file: nil,
+      repeat_file: false
+    }
+
+    {:ok, state}
   end
 
   @impl GenServer
   def handle_info({:play_file, filename}, state) do
     Task.async(fn -> System.cmd("aplay", ["/root/audio/#{filename}"]) end)
-    {:noreply, state}
+    {:noreply, state |> struct(%{file: filename})}
+  end
+
+  @impl GenServer
+  def handle_info(:set_repeat, state) do
+    {:noreply, state |> struct(%{repeat_file: true})}
   end
 
   @impl GenServer
   def handle_info(:stop_audio, state) do
     Task.async(fn -> System.cmd("killall", ["aplay"]) end)
-    {:noreply, state}
+    {:noreply, %{repeat: false}}
+    {:noreply, state |> struct(%{repeat_file: false})}
   end
 
   @impl true
@@ -40,6 +50,12 @@ defmodule NaturalWaker.SpeakerBonnet do
   @impl GenServer
   def handle_info({_ref, {"", exit_code}}, state) do
     Logger.info("Task exited with code #{exit_code}")
-    {:noreply, state}
+
+    if state.repeat_file do
+      Process.send(self(), {:play_file, state.file}, [])
+      {:noreply, state}
+    else
+      {:noreply, state |> struct(%{file: nil})}
+    end
   end
 end
