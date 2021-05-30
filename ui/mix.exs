4fbd2b587805f494878eb2212df020031c9040d3
Jason <jason@fullsteamlabs.com>
2021-05-29 20:48:42 -0400

    Updates Phoenix deps

--- ui/mix.exs
+++ ui/mix.exs
@@ -33,11 +33,11 @@ defmodule Ui.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
-      {:phoenix_live_view, "~> 0.15.0"},
+      {:phoenix, "~> 1.5.8"},
+      {:phoenix_live_view, "~> 0.15.4"},
       {:floki, ">= 0.27.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_html, "~> 2.14"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
